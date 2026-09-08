f84f652ec73fccdec80553decbc699f6cfdb45a8
Roshan Sonekar <roshansonekar@gmail.com>
2026-09-08 13:08:18 +0200

    added ORM - Dapper for booking api

---
+++ FlightBookingAPI/ABS.Booking.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,34 @@
+﻿using ABS.Booking.Core.Entities;
+using ABS.Booking.Core.Repositories;
+using Dapper;
+using System.Data;
+
+namespace ABS.Booking.Infrastructure.Repositories;
+
+public class BookingRepository : IBookingRepository
+{
+	private readonly IDbConnection _dbConnection;
+	public BookingRepository(IDbConnection dbConnection)
+	{
+		_dbConnection = dbConnection;
+	}
+
+	public async Task AddBookingAsync(BookingEntity booking)
+	{
+		const string sql = @"
+			INSERT INTO Bookings (Id, FlightId, PassangerName, SeatNumber, BookingDate)
+			VALUES (@Id, @FlightId, @PassangerName, @SeatNumber, @BookingDate)";	
+		
+		await _dbConnection.ExecuteAsync(sql, booking);
+	}
+
+	public async Task<BookingEntity> GetBookingByIdAsync(Guid id)
+	{
+		const string sql = @"
+			SELECT Id, FlightId, PassangerName, SeatNumber, BookingDate
+			FROM Bookings
+			WHERE Id = @Id";	
+
+		return await _dbConnection.QuerySingleOrDefaultAsync<BookingEntity>(sql, new { Id = id }	);
+	}
+}
